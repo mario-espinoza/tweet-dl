d56751fc3cb662176a3d4e73bee510286e211c5d
mario.espinoza@gmail.com <mario.espinoza@gmail.com@aed74aed-b796-5f5c-22f3-bf0f2b7b6cc1>
2014-04-12 16:19:22 +0000

    correccion error compilacion en build-imp.xml

--- trunk/tweet-dl/src/tweet/dl/FileExporter.java
+++ trunk/tweet-dl/src/tweet/dl/FileExporter.java
@@ -77,7 +77,7 @@ public class FileExporter {
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
             Transformer transformer = transformerFactory.newTransformer();
             DOMSource source = new DOMSource(doc);
-            StreamResult result = new StreamResult(new File("C:\\file.xml"));
+            StreamResult result = new StreamResult(new File("file.xml"));
 
             // Output to console for testing
             // StreamResult result = new StreamResult(System.out);
